f223f7801624b20f312a0f81c273dfd790c610f2
Mugil99 <mugilya99@gmail.com>
2023-07-04 11:42:55 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css'
+const Header=()=>{
+    return(
+        <header className="header"style={{borderBottom:'2px solid #FFFFFF80'}}>
+            <h3 className="header-title" style={{color:'white'}}>
+                Social Media App
+            </h3>
+        </header>
+    )
+}
+export default Header;
